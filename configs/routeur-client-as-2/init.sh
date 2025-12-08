911875e85d9b4f348edc86826e9e53ffd6ac0990
Banshai012 <menthol_assureur.0f@icloud.com>
2025-12-08 11:43:23 +0100

    Installation des nats

--- configs/routeur-client-as-2/init.sh
+++ configs/routeur-client-as-2/init.sh
@@ -1,4 +1,6 @@
 echo 1 > /proc/sys/net/ipv4/ip_forward
 ip route del default || true
 ip route add default via 120.0.32.1
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
 exec /usr/lib/frr/docker-start
