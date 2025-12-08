54b39ef715e8375e689ba7ca4e78e3d9e14070d0
hocinemediani <dolcejeannarciss@gmail.com>
2025-12-08 22:32:53 +0100

    fixed routeur-services responses and routeur-client-as-2 nat, still work to do on routeur-entreprise-as-2

--- configs/routeur-client-as-2/init.sh
+++ configs/routeur-client-as-2/init.sh
@@ -1,6 +1,8 @@
 echo 1 > /proc/sys/net/ipv4/ip_forward
 ip route del default || true
 ip route add default via 120.0.32.1
+iptables -t FORWARD DROP
+iptables -A FORWARD -m state --state ESTABLISHED, RELATED -j ACCEPT
+iptables -A FORWARD -i eth0 -o eth1 -j ACCEPT
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
 exec /usr/lib/frr/docker-start
--- configs/routeur-services/init.sh
+++ configs/routeur-services/init.sh
@@ -3,6 +3,8 @@ ip route del default || true
 ip route add default via 120.0.32.1
 # Politique par defaut, rien ne passe.
 iptables -P FORWARD DROP
+# Autoriser les reponses des services sur des connexions deja etablies.
+iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 # Communication DNS.
 iptables -A FORWARD -d 120.0.33.2 -p udp --dport 53 -j ACCEPT
 iptables -A FORWARD -d 120.0.33.2 -p tcp --dport 53 -j ACCEPT
