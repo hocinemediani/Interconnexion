26ba16c5f0ddafd1bad15d11df29c6dc2817c4c2
hocinemediani <dolcejeannarciss@gmail.com>
2025-12-08 23:03:42 +0100

    fixed routeur-services wrong source ip filter, and fixed the enterprise and client firewalls

--- configs/routeur-services/init.sh
+++ configs/routeur-services/init.sh
@@ -12,11 +12,11 @@ iptables -A FORWARD -d 120.0.33.2 -p tcp --dport 53 -j ACCEPT
 iptables -A FORWARD -d 120.0.33.2 -p icmp -j ACCEPT
 iptables -A FORWARD -d 120.0.33.3 -p icmp -j ACCEPT
 iptables -A FORWARD -s 120.0.32.3 -d 120.0.33.4 -p icmp -j ACCEPT
-iptables -A FORWARD -s 120.0.32.6 -d 120.0.33.4 -p icmp -j ACCEPT
+iptables -A FORWARD -s 120.0.32.4 -d 120.0.33.4 -p icmp -j ACCEPT
 # Requetes HTTP WEB Public.
 iptables -A FORWARD -d 120.0.33.3 -p tcp --dport 80 -j ACCEPT
 # Requetes HTTP WEB Prive depuis reseau entreprise ou client de l'AS.
 iptables -A FORWARD -s 120.0.32.3 -d 120.0.33.4 -p tcp --dport 80 -j ACCEPT
-iptables -A FORWARD -s 120.0.32.6 -d 120.0.33.4 -p tcp --dport 80 -j ACCEPT
-# Il faudra rajouter le smtp, on ne sait pas sur quel port le mettre
+iptables -A FORWARD -s 120.0.32.4 -d 120.0.33.4 -p tcp --dport 80 -j ACCEPT
+# Il faudra rajouter le smtp, on ne sait pas sur quel port le mettre.
 exec /usr/lib/frr/docker-start
