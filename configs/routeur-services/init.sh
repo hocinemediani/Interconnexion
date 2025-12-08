a8667414b4b73168dadee62340174df8556e2675
Banshai012 <menthol_assureur.0f@icloud.com>
2025-12-08 13:57:14 +0100

    iptable

--- configs/routeur-services/init.sh
+++ configs/routeur-services/init.sh
@@ -1,4 +1,16 @@
 echo 1 > /proc/sys/net/ipv4/ip_forward
 ip route del default || true
 ip route add default via 120.0.32.1
+iptables -P FORWARD DROP
+iptables -A FORWARD -d 120.0.33.2 -p udp --dport 53 -j ACCEPT
+iptables -A FORWARD -d 120.0.33.2 -p tcp --dport 53 -j ACCEPT
+iptables -A FORWARD -d 120.0.33.2 -p icmp -j ACCEPT
+iptables -A FORWARD -d 120.0.33.2 -p icmp -j ACCEPT
+iptables -A FORWARD -d 120.0.33.3 -p tcp --dport 80 -j ACCEPT
+iptables -A FORWARD -d 120.0.33.3 -p icmp -j ACCEPT
+iptables -A FORWARD -s 120.0.32.3 -d 120.0.33.4 -p tcp --dport 80 -j ACCEPT
+iptables -A FORWARD -s 120.0.32.3 -d 120.0.33.4 -p icmp -j ACCEPT # icmp pour tester le ping
+iptables -A FORWARD -s 120.0.32.6 -d 120.0.33.4 -p tcp --dport 80 -j ACCEPT
+iptables -A FORWARD -s 120.0.32.6 -d 120.0.33.4 -p icmp -j ACCEPT # icmp pour tester le ping
+# Il faudra rajouter le smtp, on ne sait pas sur quel port le mettre
 exec /usr/lib/frr/docker-start
