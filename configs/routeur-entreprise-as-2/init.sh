fae3f8df8c341336c837f02c322ca13dc376e162
hocinemediani <dolcejeannarciss@gmail.com>
2025-12-10 21:27:21 +0100

    changes comments for vpn

--- configs/routeur-entreprise-as-2/init.sh
+++ configs/routeur-entreprise-as-2/init.sh
@@ -10,16 +10,17 @@ iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
 # On remplace l'adresse ip privee des machines par l'adresse publique du routeur
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-# création du tunnel avec le protocole GRE entre 2 routeur
+
+# Creation du tunnel avec le protocole GRE entre 2 routeur.
 ip tunnel add tun0 mode gre local 120.0.32.3 remote 120.0.32.4
-# ajout d'une ip sur "l'interface" du tunnel
+# Ajout d'une ip sur "l'interface" du tunnel.
 ip link set tun0 up
 ip addr add 10.0.0.1/30 dev tun0
 ip route add 192.168.30.0/24 dev tun0
-# On autorise les entrées-sorties dans le tunnel
+# On autorise les entrees-sorties dans le tunnel.
 iptables -A FORWARD -i tun0 -j ACCEPT
 iptables -A FORWARD -o tun0 -j ACCEPT
-# autoriser les paquets encapsulées avec le protocole GRE à entrer
+# Autoriser les paquets encapsules avec le protocole GRE a entrer.
 iptables -A INPUT -p gre -j ACCEPT
 
 exec /usr/lib/frr/docker-start
