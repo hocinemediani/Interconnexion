987bbf4965878336ad9552834e5c78fa2944831d
ik389 <kraifiilian@gmail.com>
2025-12-10 11:17:05 +0100

    changed default dns server

--- configs/routeur-1/init.sh
+++ configs/routeur-1/init.sh
@@ -1,5 +1,6 @@
 echo 1 > /proc/sys/net/ipv4/ip_forward
 ip route del default || true
 ip route add 120.0.33.0/28 via 120.0.32.2
+echo "nameserver 120.0.33.2" > /etc/resolv.conf
 # il manquera les routes vers les as
 exec /usr/lib/frr/docker-start
--- configs/routeur-as-3/init.sh
+++ configs/routeur-as-3/init.sh
@@ -1,4 +1,5 @@
 echo 1 > /proc/sys/net/ipv4/ip_forward
 ip route del default || true
 ip route add default via 120.0.32.1
+echo "nameserver 120.0.33.2" > /etc/resolv.conf
 exec /usr/lib/frr/docker-start
