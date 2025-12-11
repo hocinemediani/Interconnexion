af1d42aab40b946c07f7bf32005343367c64c7b5
hocinemediani <dolcejeannarciss@gmail.com>
2025-12-11 20:48:03 +0100

    reverted dns entreprise use to the general dns of the isp

--- configs/equipement-entreprise-as-2.2/init.sh
+++ configs/equipement-entreprise-as-2.2/init.sh
@@ -1,6 +1,4 @@
 apk add dhcpcd
 ip addr flush dev eth0
-MY_HOSTNAME=$(hostname)
-udhcpc -i eth0 -x hostname:$MY_HOSTNAME
-echo "nameserver 192.168.20.8" > /etc/resolv.conf
+udhcpc -i eth0
 tail -f /dev/null
