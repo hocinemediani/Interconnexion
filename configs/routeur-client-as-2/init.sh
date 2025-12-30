5c1dae14c235c9a86be223ac97a0f7e134a15ca3
Banshai012 <menthol_assureur.0f@icloud.com>
2025-12-30 21:01:50 +0100

    VPN WireGuard

--- configs/routeur-client-as-2/init.sh
+++ configs/routeur-client-as-2/init.sh
@@ -1,3 +1,5 @@
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+apk add -U wireguard-tools
 echo 1 > /proc/sys/net/ipv4/ip_forward
 ip route del default || true
 ip route add default via 120.0.16.1
@@ -11,16 +13,31 @@ iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
 # On remplace l'adresse ip privee des machines par l'adresse publique du routeur.
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
-# Creation du tunnel avec le protocole GRE entre 2 routeur.
-ip tunnel add tun0 mode gre local 120.0.16.4 remote 120.0.16.3
-# Ajout d'une ip sur "l'interface" du tunnel.
-ip link set tun0 up
-ip addr add 10.0.0.2/30 dev tun0
-ip route add 192.168.20.0/24 dev tun0
-# On autorise les entrees-sorties dans le tunnel.
-iptables -A FORWARD -i tun0 -j ACCEPT
-iptables -A FORWARD -o tun0 -j ACCEPT
-# Autoriser les paquets encapsules avec le protocole GRE a entrer.
-iptables -A INPUT -p gre -j ACCEPT
+# Creation interface
+ip link add dev wg0 type wireguard
+ip addr add 10.0.0.2/24 dev wg0
+
+# Configuration des cles (Utilise la cle PRIVEE du Client)
+echo "qMd0No8eLidPf9HrvJRaUjQa9v7P5hnD1CPmzRq5VXU=" > /tmp/private.key
+
+# Configuration du pair (L'Entreprise)
+# Endpoint = IP Publique de l'entreprise (120.0.16.3)
+# Allowed-ips = IP Tunnel entreprise + LAN entreprise
+wg set wg0 \
+    listen-port 51820 \
+    private-key /tmp/private.key \
+    peer Y2Vc8iIzE4gl9GotUI1hZ+r4Ip/UsbJ0cj9y+jvBOAU= \
+    endpoint 120.0.16.3:51820 \
+    allowed-ips 10.0.0.1/32,192.168.20.0/24
+
+ip link set up dev wg0
+ip route add 192.168.20.0/24 dev wg0
+
+# Autoriser le trafic DANS le tunnel
+iptables -A FORWARD -i wg0 -j ACCEPT
+iptables -A FORWARD -o wg0 -j ACCEPT
+
+# Autoriser le trafic UDP chiffre a ENTRER sur l'interface publique
+iptables -A INPUT -p udp --dport 51820 -j ACCEPT
 
 exec /usr/lib/frr/docker-start
--- configs/routeur-entreprise-as-2/init.sh
+++ configs/routeur-entreprise-as-2/init.sh
@@ -1,3 +1,5 @@
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+apk add -U wireguard-tools
 echo 1 > /proc/sys/net/ipv4/ip_forward
 ip route del default || true
 ip route add default via 120.0.16.1
@@ -11,16 +13,31 @@ iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
 # On remplace l'adresse ip privee des machines par l'adresse publique du routeur.
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
-# Creation du tunnel avec le protocole GRE entre 2 routeur.
-ip tunnel add tun0 mode gre local 120.0.16.3 remote 120.0.16.4
-# Ajout d'une ip sur "l'interface" du tunnel.
-ip link set tun0 up
-ip addr add 10.0.0.1/30 dev tun0
-ip route add 192.168.30.0/24 dev tun0
-# On autorise les entrees-sorties dans le tunnel.
-iptables -A FORWARD -i tun0 -j ACCEPT
-iptables -A FORWARD -o tun0 -j ACCEPT
-# Autoriser les paquets encapsules avec le protocole GRE a entrer.
-iptables -A INPUT -p gre -j ACCEPT
+# Creation interface
+ip link add dev wg0 type wireguard
+ip addr add 10.0.0.1/24 dev wg0
+
+# Configuration des cles (Utilise la cle PRIVEE de l'Entreprise)
+echo "6P6uTnMtu9t4OX31C30UIx3SrKcvCAlMOs4T6tuiRlY=" > /tmp/private.key
+
+# Configuration du pair (Le Client)
+# Endpoint = IP Publique du client (120.0.16.4)
+# Allowed-ips = IP Tunnel du client + LAN du client
+wg set wg0 \
+    listen-port 51820 \
+    private-key /tmp/private.key \
+    peer kJu1XZnZpE+XxOjFmF1GnctTCI9fIeBqHkGp5iAySyc= \
+    endpoint 120.0.16.4:51820 \
+    allowed-ips 10.0.0.2/32,192.168.30.0/24
+
+ip link set up dev wg0
+ip route add 192.168.30.0/24 dev wg0
+
+# Autoriser le trafic DANS le tunnel
+iptables -A FORWARD -i wg0 -j ACCEPT
+iptables -A FORWARD -o wg0 -j ACCEPT
+
+# Autoriser le trafic UDP chiffre a ENTRER sur l'interface publique
+iptables -A INPUT -p udp --dport 51820 -j ACCEPT
 
 exec /usr/lib/frr/docker-start
