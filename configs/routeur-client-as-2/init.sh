a0798e51a2f45a0363d96557dd94139b79dedfbe
marouane <marouane.brd2004@gmail.com>
2026-01-07 11:49:00 +0100

    dhcp as

--- configs/routeur-client-as-2/init.sh
+++ configs/routeur-client-as-2/init.sh
@@ -1,6 +1,8 @@
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
-apk add -U wireguard-tools
+apk add -U wireguard-tools dhcpcd
 echo 1 > /proc/sys/net/ipv4/ip_forward
+ip addr flush dev eth0
+udhcpc -i eth0
 ip route del default || true
 ip route add default via 120.0.16.1
 echo "nameserver 120.0.17.3" > /etc/resolv.conf
--- configs/routeur-client-as-5/init.sh
+++ configs/routeur-client-as-5/init.sh
@@ -1,4 +1,7 @@
+apk add -U dhcpcd
 echo 1 > /proc/sys/net/ipv4/ip_forward
+ip addr flush dev eth0
+udhcpc -i eth0
 ip route del default || true
 ip route add default via 120.0.16.1
 echo "nameserver 120.0.17.3" > /etc/resolv.conf
