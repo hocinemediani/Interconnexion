624d28242113843641e3daeb54d846d28f103a86
ik389 <kraifiilian@gmail.com>
2026-01-12 11:36:23 +0100

    test_voip

--- configs/equipement-entreprise-as-2/init.sh
+++ configs/equipement-entreprise-as-2/init.sh
@@ -1,6 +1,7 @@
 apk add dhcpcd
 ip addr flush dev eth0
 udhcpc -i eth0
+
 # Installation de l'outil de test VoIP (pjsua)
 # Pour avoir un téléphone en ligne de commande
 apk update
--- configs/serveur-voip/init.sh
+++ configs/serveur-voip/init.sh
@@ -3,3 +3,4 @@ apt-get install -y iproute2 isc-dhcp-client
 ip addr flush dev eth0
 dhclient eth0
 asterisk -f
+
