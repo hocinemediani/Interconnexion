2f21ed40a6537cbbbb2f34a0b8d60d7343c2d330
ik389 <kraifiilian@gmail.com>
2026-01-13 09:45:52 +0100

    correctif voip

--- configs/admin1-entreprise-as-2/init.sh
+++ configs/admin1-entreprise-as-2/init.sh
@@ -1,9 +1,11 @@
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+# Installation de l'outil de test VoIP (pjsua)
+# Pour avoir un téléphone en ligne de commande
+apk update && apk add pjsua
 apk add dhcpcd
 ip addr flush dev eth0
 udhcpc -i eth0
 
-# Installation de l'outil de test VoIP (pjsua)
-# Pour avoir un téléphone en ligne de commande
-apk update
-apk add pjsip-tools
+echo "nameserver 192.168.20.4" > /etc/resolv.conf
+
 tail -f /dev/null
